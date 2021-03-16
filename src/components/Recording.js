fc72ba9e0cb73c5c5fdf452d73eafea2ebe12f06
genminias <gaminias@crimson.ua.edu>
2021-03-16 14:45:44 -0500

    making recordings component dynamic

--- src/components/Recording.js
+++ src/components/Recording.js
@@ -6,8 +6,11 @@ import "firebase/firestore";
 
 export function Recording () {
     const { user } = useNotion();
-    // const [snapshot, setSnapshot] = useState([]);
-    // const [recordingName, setRecordingName] = useState("");
+    const [snapshot, setSnapshot] = useState([]);
+    const [recordingName, setRecordingName] = useState("");
+    const [loading, setLoading] = useState(true);
+    // const [submitting, setSubmitting] = useState(false);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         if (!user) {
@@ -16,28 +19,40 @@ export function Recording () {
       }, [user]);
 
     useEffect(() => {
-        if (!user) {
+        if (!user) { // || submitting
             return;
         }
+
+        setLoading(true);
+
+        // we need error handling here
         getMemories();
         async function getMemories() {
             const memoriesRef = notion.__getApp().firestore().collection("memories");
-            const snapshot = await memoriesRef.where("type", "==", "epoch").where("userId", "==", user.uid).get();
-            //setSnapshot(snapshot);
+            const snapshot = await memoriesRef
+                .where("type", "==", "epoch")
+                .where("userId", "==", user.uid)
+                .get()
+                .catch((error) => { //literally no idea if this will work
+                    setError(error.message);
+                });
+            setSnapshot(snapshot);
             var select = document.getElementById("recordingSelect");
-            snapshot.forEach(doc => {
-                // const memory = doc.data();
-                // dataFunction(memory.id).then(console.log);
-                // console.log(doc.data());
-                var opt = doc.data().name;
-                var el = document.createElement("option");
-                el.textContent = opt;
-                el.value = opt;
-                select.appendChild(el);
-            });
-            console.log(snapshot.length)
+            snapshot
+                .forEach(doc => {
+                    // const memory = doc.data();
+                    // dataFunction(memory.id).then(console.log);
+                    // console.log(doc.data());
+                    var opt = doc.data().name;
+                    var el = document.createElement("option");
+                    el.textContent = opt;
+                    el.value = opt;
+                    select.appendChild(el);
+                })
+            setLoading(false);
+            console.log(snapshot.length) //test
         }
-    }, [user]);
+    }, [user, snapshot]);
 
     /* USE HTTP REQUEST to get URL to download, also don't use data() unless we want everything ???? */
 
@@ -50,13 +65,32 @@ export function Recording () {
             })
     }
 
+    function onSubmit(event) {
+        event.preventDefault();
+        // setSubmitting(true);
+        //add function that loads dataset here
+    }
+
     return(
         <main className="recordings-container">
-            <div className="row">
-                <select id="recordingSelect">
-                    <option>Choose a recording</option>
-                </select>
-            </div>
+            <form className="recordings-form" onSubmit={onSubmit}>
+                {!!error ? <h4 className="recordings-error">{error}</h4> : null}
+                <div className="row">
+                    <select
+                        id="recordingSelect"
+                        value={recordingName}
+                        disabled={loading}
+                        onChange={(e) => setRecordingName(e.target.value)}
+                    >
+                        <option>Choose a recording</option>
+                    </select>
+                </div>
+                <div className="row">
+                    <button type="submit" className="recordings-button" disabled={loading}>
+                        {loading ? "Loading Recordings..." : "Select"}
+                    </button>
+                </div>
+            </form>
         </main>
         )
-    }
+}
