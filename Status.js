6f3ce2b97013ac0637f45e9015acd0e994f2f23b
JacobN58 <37630045+JacobN58@users.noreply.github.com>
2021-03-22 15:37:17 -0500

    Add files via upload
    Moved code from Devices.js to Status.js. Remove Devices.js

---
+++ Status.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from "react";
+import { navigate } from "@reach/router";
+import { Link } from "@reach/router";
+
+import { notion, useNotion } from "../services/notion";
+
+const statesLabels = {
+  booting: "Starting OS...",
+  shuttingOff: "Shutting off...",
+  updating: "Updating OS...",
+  online: "Online",
+  offline: "Offline"
+};
+
+const stateColors = {
+  booting: "darkslategrey",
+  shuttingOff: "darkslategrey",
+  updating: "orange",
+  online: "limegreen",
+  offline: "crimson"
+};
+
+export function Status() {
+  const { status, selectedDevice } = useNotion();
+  const { state, charging, battery, sleepMode } = status || {};
+  const { user, lastSelectedDeviceId } = useNotion();
+  const [devices, setDevices] = useState([]);
+  const [draftDeviceId, setDraftDeviceId] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
+  const [error, setError] = useState("");
+
+// export function Devices() {
+//   const { user, lastSelectedDeviceId } = useNotion();
+//   const [devices, setDevices] = useState([]);
+//   const [draftDeviceId, setDraftDeviceId] = useState("");
+//   const [loading, setLoading] = useState(true);
+//   const [submitting, setSubmitting] = useState(false);
+//   const [error, setError] = useState("");
+
+
+  useEffect(() => {
+    if (!user || submitting) {
+      return;
+    }
+
+    setLoading(true);
+
+    notion
+      .getDevices()
+      .then((devices) => {
+        setDevices(devices);
+        if (devices.length) {
+          setDraftDeviceId(
+            lastSelectedDeviceId
+              ? lastSelectedDeviceId
+              : devices[0].deviceId
+          );
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [user, lastSelectedDeviceId, submitting]);
+
+  function onSubmit(event) {
+    event.preventDefault();
+    setSubmitting(true);
+
+    notion
+      .selectDevice((devices) =>
+        devices.find((device) => device.deviceId === draftDeviceId)
+      )
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }
+
+  if (!status) {
+    return <div>Connecting to device...</div>;
+  }
+
+  return (
+    <aside>
+      {selectedDevice ? (
+        <h3 className="card-heading">
+
+            <label>Select a Device </label>
+            <select
+              name="deviceSelect"
+              value={draftDeviceId}
+              disabled={loading}
+              onChange={(e) => setDraftDeviceId(e.target.value)}
+            >
+              {devices.map((device) => (
+                <option key={device.deviceId} value={device.deviceId}>
+                  {device.deviceNickname || device.modelName}
+                </option>
+              ))}
+            </select>
+
+
+
+
+            {/* <span role="img" aria-label="My Devices">
+              ⚙️
+            </span>
+            &nbsp;&nbsp;
+            {selectedDevice.deviceNickname} */}
+          {/* </Link> */}
+        </h3>
+      ) : null}
+      <div className="status-item status-state">
+        <span
+          className="status-indicator"
+          style={{ background: getStatusColor(state) }}
+        ></span>
+        {state in statesLabels ? statesLabels[state] : state}
+      </div>
+      {state !== "offline" ? (
+        <div className="status-item status-battery">
+          <ChargingIcon /> &nbsp;{charging ? "Charging" : "Charged"}
+          &nbsp;{battery}%
+        </div>
+      ) : null}
+      {sleepMode && state !== "offline" ? (
+        <div className="status-item status-sleep-mode">
+          <SleepModeIcon /> &nbsp;Sleep mode
+        </div>
+      ) : null}
+    </aside>
+  );
+}
+
+function getStatusColor(state) {
+  if (state in stateColors) {
+    return stateColors[state];
+  }
+
+  return stateColors.offline;
+}
+
+function ChargingIcon() {
+  return (
+    <span role="img" aria-label="Electricity">
+      &#x26A1;
+    </span>
+  );
+}
+
+function SleepModeIcon() {
+  return (
+    <span role="img" aria-label="The Moon">
+      &#127769;
+    </span>
+  );
+}
