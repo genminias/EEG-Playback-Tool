901909b04b73d83103daa6e760be72d1f16ecaf4
Nel Vora <ngvora@crimson.ua.edu>
2021-03-24 16:57:07 -0500

    added some chartjs code to player
    added line graph for EEG data code draft

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,30 @@
 import React from "react";
 
 export function Player() {
+    var ctx = document.getElementById("canvas").getContext("2d");
+    var eegGraph = new Chart(ctx).Line(data);
+    var data = {
+        labels: ["Channel 1", "Channel 2", "3", "4", "5", "6", "7"], //will adjust labels based on channel names from dataset, this is just a sample
+        datasets: [ //each channel will be a separate line with different colors
+            {fillColor: "rgba(220,220,220,0.2)",
+             strokeColor: "rgba(220,220,220,1)",
+             // data: [] this we will get from our global variable, this contains plot points [x,y]
+            },
+            {fillColor: "rgba(151,187,205,0.2)",
+             strokeColor: "rgba(151,187,205,1)",
+             // data: [] this we will get from our global variable, this contains plot points [x,y]
+            }
+            //... etc based on how many channels we have
+        ]};
 
 }
 
 function readData() {
+     var eegdata = { /* the global json variable */};
+     
+     lineChartData = {};             //declare an object
+     lineChartData.labels  = [];    //add 'labels' element to object (X axis)
+     lineChartData.datasets = [];  //add 'datasets' array element to object
 
 }
 
