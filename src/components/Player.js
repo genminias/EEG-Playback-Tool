14be7ba5096369645295c42deaae348b8b245c20
Nel Vora <ngvora@crimson.ua.edu>
2021-04-13 14:02:41 -0500

    plotted points on graph
    graphs now showing points for every recording

--- src/components/Player.js
+++ src/components/Player.js
@@ -12,12 +12,13 @@ export function Player() {
     const [eegSamples, dataCheck] = value1;
     var channel1 = []; var channel2 = []; var channel3 = []; var channel4 = []; 
     var channel5 = []; var channel6 = []; var channel7 = []; var channel8 = [];
+    var timestamps = [];
     const samplingRate = 250;
 
     useEffect(() => {
         const ctx = document.getElementById("myChart");
 
-        if (dataCheck == true) {
+        if (dataCheck === true) {
             console.log("made it");
             for (var i = 0; i < eegSamples.length; i++) {
                 channel1.push(eegSamples[i].data[0]);
@@ -28,16 +29,93 @@ export function Player() {
                 channel6.push(eegSamples[i].data[5]);
                 channel7.push(eegSamples[i].data[6]);
                 channel8.push(eegSamples[i].data[7]);
+                timestamps.push(i);
             }
+            console.log(eegSamples.length);
         }
 
+        var graphChannel1 = {
+            label: "Channel 1",
+            data: channel1,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+            };
+
+        var graphChannel2 = {
+            label: "Channel 2",
+            data: channel2,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+            };
+
+        var graphChannel3 = {
+            label: "Channel 3",
+            data: channel3,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+        };
+
+        var graphChannel4 = {
+            label: "Channel 4",
+            data: channel4,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+        };
+
+        var graphChannel5 = {
+            label: "Channel 5",
+            data: channel5,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+        };
+
+        var graphChannel6 = {
+            label: "Channel 6",
+            data: channel6,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+        };
+
+        var graphChannel7 = {
+            label: "Channel 7",
+            data: channel7,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+        };
+
+        var graphChannel8 = {
+            label: "Channel 8",
+            data: channel8,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'red'
+        };
+
+        var eegGraphData = {
+            labels: timestamps,
+            datasets: [graphChannel1, graphChannel2,graphChannel3,graphChannel4,graphChannel5,graphChannel6,graphChannel7,graphChannel8]
+        };
+        var chartOptions = {
+            legend: {
+                display: true,
+                position: 'top',
+                labels: {
+                boxWidth: 80,
+                fontColor: 'black'
+            }
+            }
+        };
         new Chart(ctx, {
             type: "line",
-            labels: ["label1", "label2"],
-            data: [42, 69],
-            options: {
-                maintainAspectRatio: false
-            }
+            data: eegGraphData,
+            options: chartOptions
         });
     });
 
