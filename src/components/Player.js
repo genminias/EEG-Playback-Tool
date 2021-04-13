209cd20c37923da03275eb922043bec18440fe80
Nel Vora <ngvora@crimson.ua.edu>
2021-04-13 14:36:05 -0500

    Cleaned up code
    removed unnecessary code

--- src/components/Player.js
+++ src/components/Player.js
@@ -124,21 +124,6 @@ export function Player() {
             <canvas id="myChart" width="15000" height="600" />
         </div>
     );
-    // var ctx = document.getElementById("canvas").getContext("2d");
-    // var eegGraph = new Chart(ctx).Line(data);
-    // var data = {
-    //     labels: ["Channel 1", "Channel 2", "3", "4", "5", "6", "7"], //will adjust labels based on channel names from dataset, this is just a sample
-    //     datasets: [ //each channel will be a separate line with different colors
-    //         {fillColor: "rgba(220,220,220,0.2)",
-    //          strokeColor: "rgba(220,220,220,1)",
-    //          // data: [] this we will get from our global variable, this contains plot points [x,y]
-    //         },
-    //         {fillColor: "rgba(151,187,205,0.2)",
-    //          strokeColor: "rgba(151,187,205,1)",
-    //          // data: [] this we will get from our global variable, this contains plot points [x,y]
-    //         }
-    //         //... etc based on how many channels we have
-    //     ]};
 
 }
 
@@ -146,21 +131,6 @@ function parseData(sampleInfo) {
     console.dir(sampleInfo[0]);
 }
 
-function readData() {
-   //  var eegdata = { /* the global json variable */};
-    // $.getJSON("var/eeg.json", function(data) {
-         //example of mapping json data to dataset needed for line graph
-       /* var labels = data.test_data.map(function(e) {
-           return e[0];
-        });
-        var data = data.test_data.map(function(e) {
-           return e[1]; */
-        //})
-    // lineChartData = {};             //declare an object
-    // lineChartData.labels  = [];    //add 'labels' element to object (X axis)
-    // lineChartData.datasets = [];  //add 'datasets' array element to object
-
-}
 
 function pause() {
 
