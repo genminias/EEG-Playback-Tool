00e576fed67a8a45268bb0aa14b9a0ba077557a7
JacobN58 <37630045+JacobN58@users.noreply.github.com>
2021-04-18 17:26:03 -0500

    Redid header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,34 @@ import React from "react";
  * Header component
  * Used in dashboard
  */
+const styles = {
+    fontSize: "64px",
+    color: "#231e50",
+    // flexDirection:'row',
+    // textAlign: "center",
+    // justifyContent: 'center',
+    // alignItems: 'baseline'
+    // display: 'baseline'
+    // margin: 1
+
+    // alignSelf: "stretch"
+};
+
 export function Header() {
     return (
         <div className="title">
-            <div className="dash-row">
-                <img src="src/components/logo.jpg" ></img>
-                <h1>Brainwave</h1>
+            <div className="dash-row2">
+                {/* <img src="src/components/Neurosity.png" ></img> */}
+            {/* <div style={styles}>   */}
+                <div className="dash-column">
+                <img src="http://localhost:3000/favicon.png" alt="Neurosity_Logo"/>
+                </div>
+                {/* console.log (src); */}
+            {/* </div> */}
+            <div className="dash-column">
+             <h1 style={styles}>NEUROSITY:  EEG Playback Tool</h1>
+            </div>
+
             </div>
         </div>
     );
